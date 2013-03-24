a42f88b50164c9895520e4fb3d9f0dce0c2105c1
landroni <liv-public@myopera.com>
2013-03-24 23:13:07 +0100

    misc fixes
    check that data.frame is at least 1x2, 
    allow to display only subsets of at least 1x2, 
    correctly compute dynamic label when only 1 column selected, 
    fix name of h_disp handler, 
    simplify statusbar messages

--- gWidgets2-filter.R
+++ gWidgets2-filter.R
@@ -13,9 +13,9 @@
 df_filter <- function(data_set, DF = NULL, idxs = NULL, cnms = NULL){
     library(gWidgets2) ## on github not CRAN. (require(devtools); install_github("gWidgets2", "jverzani")
     options(guiToolkit="RGtk2")
-    ## ensure we have a data frame of 2x2 dimensions
+    ## ensure we have a data frame of 1x2 dimensions
     stopifnot(is.data.frame(data_set))
-    stopifnot(all(dim(data_set) >= 2))
+    stopifnot(all(dim(data_set) >= c(1,2)))
     data_set_name <- deparse(substitute(data_set))
     
     data_set_dim_orig <- dim(data_set)
@@ -27,8 +27,9 @@ df_filter <- function(data_set, DF = NULL, idxs = NULL, cnms = NULL){
     })
     pg <- gpanedgroup(w, horizontal=TRUE)
     
-    ##!!minimal scrollwindow width
+    ##??minimal scrollwindow width
     f_side <- gvbox(cont=pg, use.scrollwindow=TRUE)
+    print(size(f_side))
     df_side <- gvbox(cont = pg, expand=TRUE)
     
     df_box <- ggroup(cont=df_side, expand=TRUE) ## holds df instance
@@ -37,8 +38,16 @@ df_filter <- function(data_set, DF = NULL, idxs = NULL, cnms = NULL){
     btn_gp <- ggroup(cont = df_side)
     do_btn <- gbutton("Merge changes...", cont=btn_gp)
     do_btn$set_icon("ok")
-    visible(do_btn) <- TRUE
     enabled(do_btn) <- FALSE
+    ##!!editable checkbox (for(j in 1:ncol(DF)) set_editable=function(j, value=FALSE))
+#     cb_do_btn <- gcheckbox('Prevent merges', checked=TRUE, cont=btn_gp, 
+#                            handler=function(h,...){
+#                                if((grepl('*', svalue(w), fixed=T) & 
+#                                        svalue(cb_do_btn))){
+#                                    enabled(do_btn) <- TRUE
+#                                }
+#     })
+#     tooltip(obj=cb_do_btn) <- "Uncheck to allow merging changes into the original data frame."
     addSpring(btn_gp)
     close_btn <- gbutton("Close", cont=btn_gp, handler=function(h,...){
         dispose(w)
@@ -47,7 +56,7 @@ df_filter <- function(data_set, DF = NULL, idxs = NULL, cnms = NULL){
     
     ## set up filters.
     ## Select columns
-    ##!!increase hight of selection box; use size()
+    ##??increase hight of selection box; use size() (bug)
     c_gp <- gframe("<b> Select columns: </b>", markup=TRUE, cont=f_side, 
                    horizontal=FALSE)
     c_names <- gcheckboxgroup(names(data_set), checked=TRUE, cont=c_gp, 
@@ -64,14 +73,20 @@ df_filter <- function(data_set, DF = NULL, idxs = NULL, cnms = NULL){
     b_clear <- gbutton("Clear", cont=ggroup(cont=s_gp), handler = function(h,...) {
         svalue(c_names, index=TRUE) <- integer()
     })
+    print(size(c_names))
     
-    ##!!check dim() bug when 'atomic'
     ## centralized handler helper fun for display button
-    b_disp <- function(h, ...) {
+    h_disp <- function(h, ...) {
         rows <- svalue(row_filter)
         cnms <<- svalue(c_names)
         idxs <<- which(rows)                  # move to global variable
-        data_set_dim <- dim(data_set[idxs, cnms])
+        ## detect size of data frame to be displayed
+        if(length(cnms)==1){
+            data_set_dim <- c(length(idxs), 1)
+        } else data_set_dim <- dim(data_set[idxs, cnms])
+        if(any(data_set_dim < c(1,2))){
+            enabled(b_disp) <- FALSE
+        } else enabled(b_disp) <- TRUE
         blockHandler(b_disp)
         ## dynamically update 'display' button label given current selection
         svalue(b_disp, append=T) <- paste('Display selection (', data_set_dim[1], 
@@ -80,26 +95,21 @@ df_filter <- function(data_set, DF = NULL, idxs = NULL, cnms = NULL){
         #font(b_disp) <- list(weight = "bold")
         unblockHandler(b_disp)
     }
-    addHandlerChanged(c_names, b_disp)
-    addHandlerChanged(b_selall, b_disp)
-    addHandlerChanged(b_clear, b_disp)
+    addHandlerChanged(c_names, h_disp)
+    addHandlerChanged(b_selall, h_disp)
+    addHandlerChanged(b_clear, h_disp)
     
     
     ## Filter rows by logical
     r_gp <- gframe("<b>Filter rows:</b>", markup=TRUE, cont=f_side, horizontal=FALSE)
     row_filter <- gfilter(data_set, cont=r_gp, expand=TRUE)
-    addHandlerChanged(row_filter, b_disp)
+    addHandlerChanged(row_filter, h_disp)
     
-    ##!!editable checkbox (for(j in 1:ncol(DF)) set_editable=function(j, value=FALSE))
-    ##!!automatic update checkbox
-    ##!!custom message when displaying full database.
     b_disp <- gbutton(paste("Display selection (", data_set_dim_orig[1], ' x ', 
                             data_set_dim_orig[2], ')', sep=''), expand=TRUE, 
                       cont=ggroup(cont=f_side), handler=function(h,...) {
-        visible(do_btn) <- TRUE
         cnms <<- svalue(c_names)
         rows <- svalue(row_filter)
-        
         
         idxs <<- which(rows)                  # move to global variable
         ## now add a data frame
@@ -118,16 +128,23 @@ df_filter <- function(data_set, DF = NULL, idxs = NULL, cnms = NULL){
             svalue(w) <- paste( substr(svalue(w), 1, (nchar(svalue(w))-1)), sep='')
         }
         data_set_dim <- dim(data_set[idxs, cnms])
-        svalue(gs_df) <- paste('Currently displaying a ', data_set_dim[1], ' x ', 
-                               data_set_dim[2], ' subset of ', data_set_name, '.', sep='')
+        ## custom message when displaying full database.
+        if(all(data_set_dim == data_set_dim_orig)){
+            svalue(gs_df) <- paste("Currently displaying the full data set.", sep='')
+        } else {
+            svalue(gs_df) <- paste('Currently displaying a ', data_set_dim[1], ' x ', 
+                               data_set_dim[2], " subset.", sep='')
+        }
     })
     enabled(b_disp) <- TRUE
     b_disp$set_icon("execute")
     #font(b_disp) <- list(weight = "bold")
+    ##!!automatic update checkbox
+    #cb_autoupdate <- gcheckbox('Update automatically', cont=ggroup(cont=f_side))
     
     size(w) <- c(600, 500)
     visible(w) <- TRUE
-    svalue(pg) <- 0.33
+    svalue(pg) <- 0.38
     
     ## What to do when you do ...
     addHandlerClicked(do_btn, function(h,...) {
@@ -147,6 +164,6 @@ df_filter <- function(data_set, DF = NULL, idxs = NULL, cnms = NULL){
     }) 
 }
 
-#require(MASS)
+require(MASS)
 Xa <- Cars93 ## this will be in a function... replace with your won
 df_filter(Xa)
